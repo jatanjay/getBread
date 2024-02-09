c1be82e75b11f9ff5d139f720e55292ed38e6a09
jatanjay <jpandya@umass.edu>
2024-02-09 00:37:11 -0500

    2/9/2024

--- XeetCode/Arrays_Hashing/product_array_except_self.py
+++ XeetCode/Arrays_Hashing/product_array_except_self.py
@@ -14,4 +14,27 @@ class Solution:
 
 """
 works , but in O(N^2), O(N) -- not the asnwer tho, they need O(N)
+
+what I learnt: 
+
+
+1. CAN JUST DO: for i in reversed([1,2,3])!
+
+
 """
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * len(nums)
+        left, right = 1, 1
+
+        for i in range(len(nums)):
+            answer[i] = left
+            left *= nums[i]
+
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] *= right
+            right *= nums[i]
+
+        return answer
